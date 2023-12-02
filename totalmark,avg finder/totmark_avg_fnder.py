9947ed71ee93d9d69a3105db00772d7eaef9831b
karthikn-7 <karthikkat7777@gmail.com>
2023-12-02 15:16:40 +0530

    total mark average finder modifiend

--- totalmark,avg finder/totmark_avg_fnder.py
+++ totalmark,avg finder/totmark_avg_fnder.py
@@ -1,7 +1,13 @@
+m1 = int(input("enter the first subject mark: "))
+m2 = int(input("enter the second subject mark: "))
+m3 = int(input("enter the three subject mark: "))
+m4 = int(input("enter the four subject mark: "))
+m5 = int(input("enter the five subject mark: "))
+
 count = 0
-mark = 0
-for marks in [90,90,90,90,99]:
+totalmark = 0
+for marks in [m1,m2,m3,m4,m5]:
     count = count +1
-    mark = mark + marks
-    # print(count,mark)
-print("Total subject:",count,"Total marks:",mark,'Average:',mark/count)
+    totalmark = totalmark+marks
+    print(count,totalmark)
+print("Total subject:",count,"Total marks:",totalmark,'Average:',totalmark/count)
