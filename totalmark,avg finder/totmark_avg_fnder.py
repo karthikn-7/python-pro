6bf51b4f17529bc98577b5ceb0ed4c16c0c9a37a
Karthikeyan <138990747+karthikn-7@users.noreply.github.com>
2023-12-02 14:14:43 +0530

    Add files via upload

---
+++ totalmark,avg finder/totmark_avg_fnder.py
@@ -0,0 +1,7 @@
+count = 0
+mark = 0
+for marks in [90,90,90,90,99]:
+    count = count +1
+    mark = mark + marks
+    # print(count,mark)
+print("Total subject:",count,"Total marks:",mark,'Average:',mark/count)
