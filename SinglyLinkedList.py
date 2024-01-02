15e85d613553cbe4fa61e23e53411742a007f6c2
Karthikeyan <138990747+karthikn-7@users.noreply.github.com>
2024-01-02 20:05:58 +0530

    Add files via upload

--- SinglyLinkedList.py
+++ SinglyLinkedList.py
@@ -1,51 +1,58 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.pointer = None
+        
 
-class SinglyLinkedList:
+class LinkedList:
+    
     def __init__(self):
-        self.__list = list()
-        
-    def add(self,element):
-        self.__element = element
-        self.__list.append(self.__element)
-        
+        self.head = None
     
-    def display(self):
-        if len(self.__list) == 0:
-            print('List is Empty!')       
+    def add(self,data):
+        Newnode = Node(data)
+        
+        if self.head is None:
+            self.head = Newnode
+        
         else:
-            print("List is:",end=' ')
-            for i in self.__list:
-                print(i,end=' ')
-        print()
-                
-    
-    def delete(self,e):
-        self.__e = e
-        if e not in self.__list:
-            print(f'{self.__e} is Not in List')
+            cur = self.head
+            while cur.pointer is not None:
+                cur = cur.pointer
             
-        else:
-            self.__list.remove(self.__e)
-        
+            cur.pointer = Newnode
     
-    def clear(self):
-        self.__list.clear()
+    def print(self):
+        cur  = self.head
         
+        while cur is not None:
+            print(cur.data,end= ' ')
+            cur = cur.pointer
+        print()
     
-    def insert(self,ind,val):
-        self.__ind = ind
-        self.__val = val
-        if self.__ind > len(self.__list):
-            print()
-            print(f'Choose index with in the list length! LIST LENGHT: {len(self.__list)}')
+    def remove(self,data):
+        
+        
+        if self.head is not None:
+            if (self.head.data == data):
+                self.head = self.head.pointer
+            else:
+                cur =self.head
+                while cur.pointer is not None and cur.pointer.data!=data:
+                    cur = cur.pointer
+                if cur.pointer is not None:
+                    cur.pointer = cur.pointer.pointer
+                    
         else:
-            self.__list.insert(self.__ind, self.__val)
-        print('----------')
+            print('linked list is empty!')
         
 
-L = SinglyLinkedList()
-L.add(20)
-L.add(30)
-L.display()
+# test code
+ll = LinkedList()
 
-        
-        
+ll.add(20)
+ll.add('string')
+ll.add(50)
+
+
+ll.print()
